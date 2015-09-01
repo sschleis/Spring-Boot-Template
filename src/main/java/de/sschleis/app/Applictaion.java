578af86071c8db1102c40b69c635ba9a331a4ca3
Sebastian Schleis <sschleis@Sebastians-MBP.fritz.box>
2015-09-02 00:50:42 +0200

    Init commit

---
+++ src/main/java/de/sschleis/app/Applictaion.java
@@ -0,0 +1,13 @@
+package de.sschleis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applictaion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Applictaion.class, args);
+    }
+
+}
